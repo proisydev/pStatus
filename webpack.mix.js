582eea245d0f4c25e6560f272934fe50e76c4559
Christopher Proisy <204402355+proisydev@users.noreply.github.com>
2025-04-13 17:03:41 +0200

    Add assets version by Mix.

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,8 +2,6 @@ import mix from "laravel-mix";
 import tailwindcss from "@tailwindcss/postcss";
 import autoprefixer from "autoprefixer";
 
-mix.js("resources/js/app.js", "public/js").postCss(
-    "resources/css/app.css",
-    "public/css",
-    [tailwindcss, autoprefixer]
-);
+mix.js("resources/js/app.js", "public/js")
+    .postCss("resources/css/app.css", "public/css", [tailwindcss, autoprefixer])
+    .version();
